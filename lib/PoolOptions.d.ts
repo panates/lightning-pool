c3511005ef8e072f5149ef64e0948ed67a819352
Eray Hanoğlu <erayszn@gmx.net>
2020-09-22 20:25:50 +0300

    Updated typing

--- lib/PoolOptions.d.ts
+++ lib/PoolOptions.d.ts
@@ -12,4 +12,3 @@ export declare class PoolOptions {
     public resetOnReturn: boolean;
     public validation: boolean;
 }
-
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,13 +1,10 @@
 import Pool, {IPoolFactory} from './Pool';
 import type {PoolOptions} from './PoolOptions';
 
-declare module 'lightning-pool' {
+export {IPoolFactory, PoolState} from './Pool';
+export {PoolOptions};
 
-    export {IPoolFactory, PoolState} from './Pool';
-    export {PoolOptions};
-
-    export class AbortError extends Error {
-    }
-
-    export function createPool(factory: IPoolFactory, options: PoolOptions): Pool;
+export class AbortError extends Error {
 }
+
+export function createPool(factory: IPoolFactory, options: PoolOptions): Pool;
