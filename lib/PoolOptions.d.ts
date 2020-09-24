ea05c8bb2cd2b37f3702d0321107f6eb150d0c1e
Eray Hanoğlu <erayszn@gmx.net>
2020-09-24 22:33:49 +0300

    Updated typing

--- lib/PoolOptions.d.ts
+++ lib/PoolOptions.d.ts
@@ -1,14 +1,14 @@
 export declare class PoolOptions {
-    public acquireMaxRetries: number;
-    public acquireRetryWait: number;
-    public acquireTimeoutMillis: number;
-    public fifo: boolean;
-    public idleTimeoutMillis: number;
-    public houseKeepInterval: number;
-    public min: number;
-    public minIdle: number;
-    public max: number;
-    public maxQueue: number;
-    public resetOnReturn: boolean;
-    public validation: boolean;
+    public acquireMaxRetries?: number;
+    public acquireRetryWait?: number;
+    public acquireTimeoutMillis?: number;
+    public fifo?: boolean;
+    public idleTimeoutMillis?: number;
+    public houseKeepInterval?: number;
+    public min?: number;
+    public minIdle?: number;
+    public max?: number;
+    public maxQueue?: number;
+    public resetOnReturn?: boolean;
+    public validation?: boolean;
 }
