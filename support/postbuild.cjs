59883083b43aa0cfccb2116115e5d26bd082ed42
Eray Hanoğlu <e.hanoglu@panates.com>
2024-08-20 20:31:25 +0300

    Fixed compatibility for "Node16" and "NodeNext" moduleResolution options

--- support/postbuild.cjs
+++ support/postbuild.cjs
@@ -2,6 +2,8 @@ const fs = require('node:fs');
 const path = require('node:path');
 
 function clearPackageJson() {
+  const targetPath = path.resolve(__dirname, '../build');
+
   const json = JSON.parse(
     fs.readFileSync(path.resolve(__dirname, '../package.json'), 'utf-8'),
   );
@@ -9,8 +11,12 @@ function clearPackageJson() {
   delete json.scripts;
   delete json.devDependencies;
   fs.writeFileSync(
-    path.resolve(__dirname, '../build/package.json'),
+    path.resolve(targetPath, 'package.json'),
     JSON.stringify(json, undefined, 2),
+  );
+  fs.copyFileSync(
+    path.resolve(targetPath, './types/index.d.ts'),
+    path.resolve(targetPath, './types/index.d.cts'),
   );
 }
 
