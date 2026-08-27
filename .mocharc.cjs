5d6bbc4839d2f22565ea2921a53dabb37bdd40db
Eray Hanoğlu <e.hanoglu@panates.com>
2026-08-27 15:48:14 +0300

    chore: Removed unused configs, updated build process, and upgraded dependencies

--- .mocharc.cjs
+++ .mocharc.cjs
@@ -1,7 +1,12 @@
 process.env.TS_NODE_PROJECT = __dirname + '/test/tsconfig.json';
+
 /** @type {import('mocha').MochaOptions} */
 module.exports = {
-  require: ['@swc-node/register/esm-register'],
+  require: [
+    '@swc-node/register/esm-register',
+    __dirname + '/test/_support/env.ts',
+    __dirname + '/test/_support/create-db.ts',
+  ],
   extension: ['ts'],
   spec: './test/**/*.spec.ts',
   timeout: 30000,
